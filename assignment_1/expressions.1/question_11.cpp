019a997a3443bb04b396d38c68aa17f4adfda712
Manisha55-02 <nakkamanisha55@gmail.com>
2022-12-03 16:20:10 +0530

    new
    new

---
+++ assignment_1/expressions.1/question_11.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int height,vi=0,vf;
+    int ad=9.8;
+    char units;
+    cin>>height>>units;
+    cout<<"vf= "<<sqrt(vi*vi+2*ad);
+
+    }
